449b0684241d173e791f32e6b726d8e5e85397bf
Veera Peltomäki <veera.peltomaki@avidlyagency.com>
2022-06-17 11:50:51 +0300

    Detect parent DOM element from click and sen info to dataLayer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 // Run when DOM is loaded.
 document.addEventListener('DOMContentLoaded', function(event) {
 	gtm4wpEventClick();
-})
+});
 
 /**
  * Detect clicks for data attribute & send info to dataLayer.
@@ -12,11 +12,57 @@ function gtm4wpEventClick() {
 	if ( items ) {
 		for (i of items) {
 			i.addEventListener( 'click', function(e) {
+
+				// Find specific DOM element from element parents.
+				let findDOM = [ 'HEADER', 'FOOTER', 'ASIDE', 'SECTION' ];
+				let parent = matchValue( getAncestors(this), findDOM );
+
+				// Add click realted stuff to dataLayer.
 				dataLayer.push({
 					'wp_click_type': this.getAttribute('data-click-type'),
-					'wp_click_event': this.getAttribute('data-click-event')
+					'wp_click_event': this.getAttribute('data-click-event'),
+					'wp_click_dom': parent
 				});
+
 			});
 		}
 	}
+}
+
+/**
+ * Get all element ancestors nodeName.
+ * @returns ancestors
+ */
+const getAncestors = el => {
+	let ancestors = [];
+
+	while (el) {
+		el = el.parentNode;
+
+		// Get only nodeNames from ancestors.
+		if ( null !== el ){
+			ancestors.unshift(el.nodeName);
+		}
+	}
+
+	return ancestors;
+};
+
+/**
+ * Detect if array contains specific values.
+ *
+ * @param array arr array where to find matches.
+ * @param array find values to find.
+ * @returns 
+ */
+function matchValue( arr, find ) {
+	let domElement = '';
+
+	find.forEach(value => {
+		if ( arr.includes(value) ) {
+			domElement = value	
+		}
+	});
+
+	return domElement;
 }
